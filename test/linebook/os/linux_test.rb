81ef4c712b462d4f42e412f5e28423017171fccd
Simon Chiang <simon.chiang@pinnacol.com>
2011-03-03 14:12:08 -0700

    sketched out useradd

---
+++ test/linebook/os/linux_test.rb
@@ -0,0 +1,42 @@
+require File.expand_path('../../../test_helper', __FILE__)
+require 'linebook/os/linux'
+
+class LinuxTest < Test::Unit::TestCase
+  include Linecook::Test
+  
+  def setup
+    super
+    setup_host 'abox'
+    setup_helpers Linebook::Os::Linux
+  end
+  
+  TEST_USER  = 'test_user'
+  TEST_GROUP = 'test_group'
+  
+  def clean_recipe
+    setup_recipe do
+      target.puts "sudo userdel #{TEST_USER} > /dev/null 2>&1"
+      target.puts "sudo groupdel #{TEST_GROUP} > /dev/null 2>&1"
+      target.puts "true"
+    end
+  end
+  
+  #
+  # useradd test
+  #
+  
+  def test_useradd_adds_user
+    clean_recipe
+    
+    setup_recipe do
+      target.puts "id #{TEST_USER} 2>&1"
+      useradd TEST_USER
+      target.puts "id -nu #{TEST_USER}"
+    end
+    
+    assert_output_equal %{
+      id: #{TEST_USER}: No such user
+      #{TEST_USER}
+    }, *run_package
+  end
+end
