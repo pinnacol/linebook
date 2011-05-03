d217f9b1a37d46fd38d85eb5c207fd52879502ab
Simon Chiang <simon.chiang@pinnacol.com>
2011-05-03 13:52:10 -0600

    added a little doc to Posix

---
+++ helpers/linebook/os/posix/-doc.rb
@@ -0,0 +1,9 @@
+# Defines basic POSIX utilities, and POSIX-compliant functionality. See the
+# online documentation of:
+#
+# * {POSIX Shell Command
+#   Language}[http://pubs.opengroup.org/onlinepubs/009695399/utilities/xcu_cha
+#   p02.html]
+# * {Special Built-in
+#   Utilites}[http://pubs.opengroup.org/onlinepubs/009695399/idx/sbi.html]
+#
